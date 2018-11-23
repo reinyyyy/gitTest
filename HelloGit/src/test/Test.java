89a7696869040cd23fd665568d33417777178787
minggul <forrest_gump_@naver.com>
2018-11-23 16:43:45 +0900

    sysout 추가함

--- HelloGit/src/test/Test.java
+++ HelloGit/src/test/Test.java
@@ -5,5 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		System.out.println("¾È³çÇÏ¼¼¿©");
+		System.out.println("Â÷¹Î¿ì¿Ó´Ù°¨");
 	}
 }
